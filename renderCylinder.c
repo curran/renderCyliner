a67e5d8595965d91a624710da84744d6ed9bd7b7
Joost <joostrijneveld@gmail.com>
2012-10-08 15:21:08 +0300

    Whitespace inconsistency - oops!

--- renderCylinder.c
+++ renderCylinder.c
@@ -25,47 +25,47 @@
 void renderCylinder(float x1, float y1, float z1, float x2,float y2, float z2, float radius,int subdivisions,GLUquadricObj *quadric)
 {
   float vx = x2-x1;
-	float vy = y2-y1;
-	float vz = z2-z1;
-	float v = sqrt( vx*vx + vy*vy + vz*vz );
-	float ax;
+  float vy = y2-y1;
+  float vz = z2-z1;
+  float v = sqrt( vx*vx + vy*vy + vz*vz );
+  float ax;
 
-	if (fabs(vz) < 1.0e-3) {
-		ax = 57.2957795*acos( vx/v ); // rotation angle in x-y plane
-		if ( vy <= 0.0 )
-			ax = -ax;
-	}
-	else {
-		ax = 57.2957795*acos( vz/v ); // rotation angle
-		if ( vz <= 0.0 )
-			ax = -ax;
-	}
-	
-	float rx = -vy*vz;
-	float ry = vx*vz;
-	
-	glPushMatrix();
-	//draw the cylinder body
-	glTranslatef( x1,y1,z1 );
-	if (fabs(vz) < 1.0e-3) {
-		glRotated(90.0, 0, 1, 0.0); // Rotate & align with x axis
-		glRotated(ax, -1.0, 0.0, 0.0); // Rotate to point 2 in x-y plane
-	}
-	else {
-		glRotated(ax, rx, ry, 0.0); // Rotate about rotation vector
-	}
-	gluQuadricOrientation(quadric,GLU_OUTSIDE);
-	gluCylinder(quadric, radius, radius, v, subdivisions, 1);
+  if (fabs(vz) < 1.0e-3) {
+    ax = 57.2957795*acos( vx/v ); // rotation angle in x-y plane
+    if ( vy <= 0.0 )
+      ax = -ax;
+  }
+  else {
+    ax = 57.2957795*acos( vz/v ); // rotation angle
+    if ( vz <= 0.0 )
+      ax = -ax;
+  }
+  
+  float rx = -vy*vz;
+  float ry = vx*vz;
+  
+  glPushMatrix();
+  //draw the cylinder body
+  glTranslatef( x1,y1,z1 );
+  if (fabs(vz) < 1.0e-3) {
+    glRotated(90.0, 0, 1, 0.0); // Rotate & align with x axis
+    glRotated(ax, -1.0, 0.0, 0.0); // Rotate to point 2 in x-y plane
+  }
+  else {
+    glRotated(ax, rx, ry, 0.0); // Rotate about rotation vector
+  }
+  gluQuadricOrientation(quadric,GLU_OUTSIDE);
+  gluCylinder(quadric, radius, radius, v, subdivisions, 1);
 
-	//draw the first cap
-	gluQuadricOrientation(quadric,GLU_INSIDE);
-	gluDisk( quadric, 0.0, radius, subdivisions, 1);
-	glTranslatef( 0,0,v );
+  //draw the first cap
+  gluQuadricOrientation(quadric,GLU_INSIDE);
+  gluDisk( quadric, 0.0, radius, subdivisions, 1);
+  glTranslatef( 0,0,v );
 
-	//draw the second cap
-	gluQuadricOrientation(quadric,GLU_OUTSIDE);
-	gluDisk( quadric, 0.0, radius, subdivisions, 1);
-	glPopMatrix();
+  //draw the second cap
+  gluQuadricOrientation(quadric,GLU_OUTSIDE);
+  gluDisk( quadric, 0.0, radius, subdivisions, 1);
+  glPopMatrix();
 }
 
 void renderCylinder_convenient(float x1, float y1, float z1, float x2,float y2, float z2, float radius,int subdivisions)
